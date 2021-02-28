97c6b164bc14ac4f9d19f851b3c78ea08d52d859
HUYEN NGUYEN <nguyenchauhuyen@gmail.com>
2021-02-28 13:01:56 +0700

    first commmit

---
+++ src/authentication/tests/user.mock.ts
@@ -0,0 +1,10 @@
+import User from '../../users/user.entity';
+
+const mockedUser: User = {
+  id: 1,
+  email: 'user@email.com',
+  name: 'John',
+  password: 'hash'
+}
+
+export default mockedUser;
